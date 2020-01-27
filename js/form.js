ebf65709bb56fd98989fad91dba3293b2e4caaa6
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2020-01-27 09:52:30 -0300

    Initial commit

---
+++ js/form.js
@@ -0,0 +1,21 @@
+function sendForm(event) {
+    event.preventDefault();
+    $('#recaptcha').val(grecaptcha.getResponse());
+    
+    $('.alert').css('display', 'none');
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:5000/email/send/',
+        data: $('form').serialize(),
+        dataType: 'JSON',
+        success: function (result) {
+            window.scrollTo(0, 0);
+            $('form')[0].reset();
+            $('form').removeClass('was-validated');
+            $('#alert-success').css('display', 'flex');
+        },
+        error: function (error) {
+            $('#alert-danger').css('display', 'flex');
+        },
+    });
+}
