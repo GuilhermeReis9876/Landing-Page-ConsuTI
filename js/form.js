063caf20844aa9ec8d69554e0153e2b984e47eef
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2020-01-27 13:51:33 -0300

    Recaptcha

--- js/form.js
+++ js/form.js
@@ -1,18 +1,25 @@
 function sendForm(event) {
     event.preventDefault();
+
+    $('#msg-recaptcha').css('display', 'none');
     $('#recaptcha').val(grecaptcha.getResponse());
-    
     $('.alert').css('display', 'none');
     $.ajax({
         type: 'POST',
-        url: 'https://apiconsuti.herokuapp.com/email/send/',
+        url: 'http://localhost:8080/email/send/',
         data: $('form').serialize(),
         dataType: 'JSON',
         success: function (result) {
+            if(result.includes('success')) {
+                $('form')[0].reset();
+                $('form').removeClass('was-validated');
+                $('#alert-success').css('display', 'flex');
+            }
+            else {
+                $('#alert-danger').css('display', 'flex');
+                $('#alert-danger').html(result);
+            }
             window.scrollTo(0, 0);
-            $('form')[0].reset();
-            $('form').removeClass('was-validated');
-            $('#alert-success').css('display', 'flex');
         },
         error: function (error) {
             $('#alert-danger').css('display', 'flex');
